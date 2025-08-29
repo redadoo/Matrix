20061809e50aa61c8b1c4bdb21d460e0479ce431
redadoo <edoardovocaturo@gmail.com>
2025-08-29 12:51:21 +0200

    fix typo

--- src/utils/matrix_utils.hpp
+++ src/utils/matrix_utils.hpp
@@ -90,12 +90,13 @@ namespace Maft
 	template<typename T>
 	Matrix<4, 4, T> translate(const Matrix<4, 4, T>& m, const Vector<3, T>& v)
 	{
-		Matrix<4, 4, T> Result(m);
+		Matrix<4, 4, T> result(m);
 		
 		result(3, 0) = m(0, 0) * v.x + m(1, 0) * v.y + m(2, 0) * v.z + m(3, 0);
 		result(3, 1) = m(0, 1) * v.x + m(1, 1) * v.y + m(2, 1) * v.z + m(3, 1);
 		result(3, 2) = m(0, 2) * v.x + m(1, 2) * v.y + m(2, 2) * v.z + m(3, 2);
 		result(3, 3) = m(0, 3) * v.x + m(1, 3) * v.y + m(2, 3) * v.z + m(3, 3);
-		return Result;
+		
+		return result;
 	}
 } 
